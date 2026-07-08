849e02805f42dfb18a1f23ffb52fd7d804071330
idinium96 <47635037+idinium96@users.noreply.github.com>
2026-07-08 14:19:52 +0800

    🔨 small fix

--- public/js/getMptfPrice.js
+++ public/js/getMptfPrice.js
@@ -1,13 +1,42 @@
-const xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-        const json = JSON.parse(this.responseText);
+// Function wrapper handles local scoping perfectly
+function makeRequest(url, callback) {
+    const xhr = new XMLHttpRequest(); // Unique local instance for each call
+    xhr.open('GET', url, true);
+    
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            callback(null, JSON.parse(xhr.responseText));
+        } else {
+            callback(`Error: ${xhr.status}`);
+        }
+    };
+    
+    xhr.send();
+}
+
+const bptfQuery = document.getElementById('getMptfPrice').getAttribute('bptfQuery');
+const url1 = `https://api.backpack.tf/item/get_third_party_prices/${bptfQuery}`
+makeRequest(url1, (err, json) => {
+    if (!err) {
         const price = json.prices?.mp?.lowest_price;
         const hasPrice = price && !Array.isArray(price);
         document.getElementById('mptfLink').innerText = 'Marketplace.tf' + `${hasPrice ? ` (${price})` : ''}`;
+    };
+});
+
+const sku = document.getElementById('getMptfPrice').getAttribute('sku');
+const url2 = `https://pricedb.io/api/item/${sku}`;
+
+makeRequest(url2, (err, json) => {
+    if (!err) {
+        const price = json.sell;
+        const hasPrice = !!price;
+        document.getElementById('pricedbLink').innerText = hasPrice
+            ? `Pricedb.io (${
+                  !!price.keys
+                      ? `${price.keys} key${price.keys > 1 ? 's' : ''}${!!price.metal ? `, ${price.metal} ref` : ''}`
+                      : `${price.metal} ref`
+              })`
+            : 'Pricedb.io';
     }
-};
-const bptfQuery = document.getElementById('getMptfPrice').getAttribute('bptfQuery');
-xhr.open('get', `https://api.backpack.tf/item/get_third_party_prices/${bptfQuery}`, true);
-xhr.setRequestHeader('Content-Type', 'application/json');
-xhr.send();
+});
