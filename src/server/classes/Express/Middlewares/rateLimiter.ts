e24c22eee7514733dcbff67a8afb722e6f0ea5fc
idinium96 <47635037+idinium96@users.noreply.github.com>
2026-07-18 00:05:50 +0800

    🔄️ upgrade express-rate-limit

--- src/server/classes/Express/Middlewares/rateLimiter.ts
+++ src/server/classes/Express/Middlewares/rateLimiter.ts
@@ -1,12 +1,12 @@
 // Temporary rate-limit implementation
 // Reference: https://blog.logrocket.com/rate-limiting-node-js/
 
-import rateLimit from 'express-rate-limit';
+import { rateLimit } from 'express-rate-limit';
 
 export const rateLimiterUsingThirdParty = rateLimit({
-    windowMs: 1000,
-    max: 5,
+    windowMs: 1000, // 1 second
+    max: 5, // Limit each IP to 5 requests per `windowMs`
     message: 'You have exceeded the 5 requests/second limit!',
-    standardHeaders: true,
+    standardHeaders: 'draft-7',
     legacyHeaders: false
 });
